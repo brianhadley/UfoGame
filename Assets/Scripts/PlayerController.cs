45c5a5b84c32b6bdbfdbaa08d812e99df021bdf9
brianhadley <pebkak@gmail.com>
2016-07-17 16:20:23 -0700

    vid 3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed;
+    private Rigidbody2D rb2d;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }	
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 v2 = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(v2 * Speed);
+            
+        }
+}
