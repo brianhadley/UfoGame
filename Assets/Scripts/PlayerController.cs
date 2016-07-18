3a744fdc6c78f5d69fcc8fcd9ca302d3905b2708
brianhadley <pebkak@gmail.com>
2016-07-17 22:55:07 -0700

    through 8, with some additions/mods to tutorial functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float Speed;
+    public GameObject mainController;
     private Rigidbody2D rb2d;
-    
+    private int TrackTakeOff = 240;
+
+        
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        
     }	
 	
 	// Update is called once per frame
@@ -16,8 +20,18 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector2 v2 = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(v2 * Speed);
+
+
+        Vector3 v3 = new Vector3(moveHorizontal, moveVertical,50);
+        rb2d.AddForce(v3 * Speed);
+        //gameObject.transform.localScale = new Vector3(2, 2);
             
         }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        MasterScript mainScript = mainController.GetComponent<MasterScript>();
+
+        mainScript.CollectableEncountered(other);
+    }
 }
