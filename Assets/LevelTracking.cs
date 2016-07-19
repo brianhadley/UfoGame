451ab5929d3ac66f4f1d99964c36734850e582da
brianhadley <pebkak@gmail.com>
2016-07-18 19:18:20 -0700

    becase sourcetree said?

---
+++ Assets/LevelTracking.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTracking : MonoBehaviour {
+
+   public void Enable()
+   {
+        foreach (Collider2D collider in gameObject.GetComponents<Collider2D>())
+        {
+            collider.enabled=collider.isTrigger?true:false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+             foreach (Collider2D collider in gameObject.GetComponents<Collider2D>())
+             {
+                collider.enabled = collider.isTrigger ? false : true;
+            }
+        }           
+       
+        
+    }
+}
---
+++ Assets/MasterScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterScript : MonoBehaviour {
+
+    public GameObject CurrentLevel;
+    public GameObject Player;
+
+    public void CollectableEncountered(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+
+        }
+
+        int curLevelActive = 0;
+        foreach (Transform child in CurrentLevel.transform.FindChild("Pickups"))
+        {
+            if (child.tag == "PickUp" && child.gameObject.activeSelf)
+            {
+                curLevelActive++;
+            }
+        }
+
+        if (curLevelActive == 0)
+        {
+            foreach (BoxCollider2D collider in CurrentLevel.GetComponents<BoxCollider2D>())
+            {
+                collider.enabled = false;
+            }
+
+            LevelTracking lt = CurrentLevel.GetComponent<LevelTracking>();
+            CurrentLevel.tag = "Completed Level";
+
+            GameObject nextLevel = GameObject.FindGameObjectWithTag("Level");
+
+            nextLevel.GetComponent<LevelTracking>().Enable();
+            CurrentLevel = nextLevel;
+
+        }
+
+
+
+    }
+	
+}
